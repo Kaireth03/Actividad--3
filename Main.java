8bf248bf09a088178a2a4c889908a118220ba4b9
Kaireth03 <153779015+Kaireth03@users.noreply.github.com>
2025-04-25 00:59:56 -0500

    Se realiza codigo de actividad 3

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String Nom = "Andres Rueda";
+        int Ced = 8925719;
+        byte Edad = 18;
+        boolean Residente = false;
+        double Piso = 2.6;
+        float Estatura = 1.70F;
+        long ID = 13564210481L;
+
+       System.out.println("Datos del visitante:");
+       System.out.println("Nombre: " + Nom);
+       System.out.println("Cedula: " + Ced);
+       System.out.println("Edad: " + Edad);
+       System.out.println("Residente: " + Residente);
+       System.out.println("Torre,piso: " + Piso);
+       System.out.println("Altura: " + Estatura);
+       System.out.println("id: " + ID);
+    }
+}
