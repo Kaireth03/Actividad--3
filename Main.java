4c882481239ffb941f1407e9f0fe193962b0c0ad
Kaireth03 <153779015+Kaireth03@users.noreply.github.com>
2025-04-25 01:07:52 -0500

    Changes to be committed: 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+//Información del visitante
         String Nom = "Michael";
         int Ced = 8925719;
         byte Edad = 27;
-        boolean Residente = false;
+        boolean Residente = true;
         double Piso = 5;
         float Estatura = 1.80F;
+        char Torre = 'B'; //Torre de complejos
+        short Apartment = 304;
         long ID = 13564210481L;
 
        System.out.println("Datos del visitante:");
